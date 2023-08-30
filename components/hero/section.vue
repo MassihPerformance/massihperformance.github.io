f8d97110c032701fc5c0eb0b39b7dbb70d40e6a3
NoahOnFyre <noahonfyre@gmail.com>
2023-08-30 08:35:26 +0200

    Fix buttons predefined in hero instead of variable.

--- components/hero/section.vue
+++ components/hero/section.vue
@@ -4,25 +4,6 @@
             <Eyebrow class="-mb-2">{{ eyebrow }}</Eyebrow>
             <Heading class="text-5xl">{{ heading }}</Heading>
             <slot></slot>
-            <div class="flex gap-2">
-                <WidgetButton>
-                    <span>Jetzt anfangen</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-4 h-4">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                    </svg>
-                </WidgetButton>
-
-                <WidgetLink>
-                    <span>Informationen</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-4 h-4">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                    </svg>
-                </WidgetLink>
-            </div>
         </div>
     </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,29 @@
             changes or features, check out our <Href dest="https://github.com/massihperformance">GitHub repository</Href>
             and feel free to open an issue or a pull request.
         </Text>
+
+        <div class="flex gap-2">
+            <NuxtLink to="https://github.com/massihperformance">
+                <WidgetButton>
+                    <span>Get started</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                    </svg>
+                </WidgetButton>
+            </NuxtLink>
+
+            <NuxtLink to="https://github.com/massihperformance/massihperformance.github.io">
+                <WidgetLink>
+                    <span>Informationen</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                    </svg>
+                </WidgetLink>
+            </NuxtLink>
+        </div>
     </HeroSection>
 </template>
